b13c3c651c1a61af65cf22eb9128a6caf6043470
JoelSaleem <40210575+JoelSaleem@users.noreply.github.com>
2020-04-12 16:06:46 +0100

    Updated test names

--- suadelabs/reporter/test_views.py
+++ suadelabs/reporter/test_views.py
@@ -8,26 +8,26 @@ def test_date_valid_length():
     assert is_date_valid('1994-12-11') == True
 
 
-def test_year_length():
+def test_validation_year_length():
     '''Tests whether the year is the two digits long'''
     assert is_date_valid('199-12-11') == False
     assert is_date_valid('1994-12-11') == True
 
 
-def test_month_length():
+def test_validation_month_length():
     '''Tests whether the month is the two digits long'''
     assert is_date_valid('1994-123-11') == False
     assert is_date_valid('1994-12-11') == True
 
 
-def test_day_length():
+def test_validation_day_length():
     '''Tests whether the day is two digits long'''
     assert is_date_valid('1994-12-1') == False
     assert is_date_valid('1994-12-121') == False
     assert is_date_valid('1994-12-11') == True
 
 
-def test_date_integers():
+def test_validation_date_integers():
     '''Tests whether the year, month and day are integers'''
     assert is_date_valid('AAAA-12-01') == False
     assert is_date_valid('1994-AA-01') == False
@@ -35,7 +35,7 @@ def test_date_integers():
     assert is_date_valid('1994-12-01') == True
 
 
-def test_str_input():
+def test_validation_str_input():
     '''Tests whether the function returns False for incorrect input type'''
     assert is_date_valid(19941211) == False
     assert is_date_valid('1994-12-11') == True
